21d8d8a9bce8978b43565d47f594b7dbc29de108
Yareek95 <yori95ovsii@gmail.com>
2023-11-01 19:25:03 -0500

    test added time

--- test_001.py
+++ test_001.py
@@ -1,4 +1,6 @@
 import re
+import time
+
 from playwright.sync_api import Page, expect
 
 def test_has_title(page: Page):
@@ -20,5 +22,5 @@ def test_has_credentials(page: Page):
 def test_login(page: Page):
     page.goto("https://opensource-demo.orangehrmlive.com")
     page.get_by_role("button", name="Login").click()
-
     expect(page.locator("//div[@class='orangehrm-login-slot-wrapper']//div[1]//div[1]//span[1]")).to_have_text("Required")
+    time.sleep(2)
